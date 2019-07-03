90dea4c2279dbde5ef8de2281d5e29aaae7765f8
Steven G. Harms <steven.harms@flatironschool.com>
2019-07-03 11:18:44 -0400

    Add return statement

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -8,5 +8,5 @@ def meal_choice(veg1, veg2, protein='meat')
   puts "What a nutritious meal!"
   meal = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts meal
-  meal
+  return meal # return here is optional. Ruby methods automatically return the last expression
 end
